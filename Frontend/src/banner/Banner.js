d65d72a8cd97f685dff57eb79e515fd820b3aa8b
priyanshugoel <priyanshugoel001@gmail.com>
2024-04-28 00:02:56 +0530

    Create Table to Display data from the Excel

--- Frontend/src/banner/Banner.js
+++ Frontend/src/banner/Banner.js
@@ -1,43 +1,86 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import './Banner.css';
-const Banner = () => {
-  return (
-    <section class="banner-section d-flex flex-column justify-content-center align-items-center" id="home">
-      <div className="container">
-        <div className="row inner-container">
-          <div className="col-lg-5 col-md-5 col-12 container-col">
-            <div class="banner-items mt-5">
-              <h1 class="text-black" data-aos="fade-up" data-aos-delay="500">
-                Employee Listening and Coaching Tool
-              </h1>
-              <h4>
-                Achieve more with less by streamlining technology, processes and People
-              </h4>
-              <br></br>
-              <Link to="/demo">
-                <button className="button-navigation w-button ">Request a Demo</button>
-              </Link>
-            </div>
-          </div>
-          <div className="col-lg-5 col-md-5 col-12 container-col">
-            <div class="banner-items mt-5">
-              <h1 class="text-black" data-aos="fade-up" data-aos-delay="500">
-                Join the <br></br>community
-              </h1>
-              <h4>
-                Meet visionary CPOs and HRs sharing best practices with each other
-              </h4>
-              <br></br>
-              <Link to="/apply">
-                <button className="button-navigation w-button ">Apply for Access</button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import axios from 'axios';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
-export default Banner;
+export default function Banner() {
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('/allData'); // Assuming your API endpoint is '/allData'
+      setRowData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const fieldMap = {
+    sno: 'SNO',
+    unit: 'Unit',
+    typeOfEquipment: 'Type of Eqpt',
+    issueType: 'Issue Type',
+    baNo: 'BA No',
+    chassisNo: 'Chassis No',
+    engineOrgOH: 'Engine Org/OH',
+    engKm: 'Eng Km',
+    engHrs: 'Eng Hrs',
+    chassisKm: 'Chassis Km',
+    chassisHrs: 'Chassis Hrs',
+    tmIDone: 'TM I Done',
+    tmIDue: 'TM I Due',
+    tmIIDone: 'TM II Done',
+    tmIIDue: 'TM II Due',
+    mrIDueDt: 'MR-I Due Dt',
+    mrIDoneDt: 'MR-I Done Dt',
+    ohIDueDt: 'OH-I Due Dt',
+    ohIDoneDt: 'OH-I Done Dt',
+    mrIIDue: 'MR II Due',
+    mrIIDone: 'MR II Done',
+    ohIIDue: 'OH II Due',
+    ohIIDone: 'OH II Done',
+    serR2EOAVOR: 'SER/R2/EOA/VOR',
+    assy: 'Assy',
+    section: 'Section',
+    natureOfDefect: 'Nature of Defect',
+    demandPlacedTo: 'Demand Placed To',
+    demandNoDt: 'Demand No & Dt',
+    contNoDt: 'Cont No & Dt',
+    workOrderNoDate: 'Work Order No & Dt',
+    fwdTo: 'Fwd To',
+    since: 'Since',
+    presentStatus: 'Present Status',
+    underRepairTime: 'Under Repair Time',
+    efcRDSFired: 'EFC/RDS Fired',
+    chamberElongation: 'Chamber Elongation',
+    bore: 'Bore',
+    gunPullBackDoneDate: 'Gun Pull Back Done Date',
+    siDetails: 'SI Details',
+    fumeExtractor: 'Fume Extractor',
+    n2PurgingDueDate: 'N2 Purging Due Date',
+    n2PurgingCarriedOut: 'N2 Purging Carried Out',
+    getterActivationDoneDate: 'Getter Activation Done Date (Check Every 3 Months)'
+  };
+
+  const columnDefs = Object.values(fieldMap).slice(0, 5).map(key => ({
+    field: key,
+    headerName: key,
+    sortable: true,
+    filter: true
+  }));
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: '500px', width: '100%' }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        pagination={true}
+      />
+    </div>
+  );
+}
