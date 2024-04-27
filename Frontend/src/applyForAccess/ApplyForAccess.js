fd82d88d7ecfb0935267c1227ace00e596e7aa6c
UserName <UserEmail>
2024-04-27 22:25:01 +0530

    Minor UI change

--- Frontend/src/applyForAccess/ApplyForAccess.js
+++ Frontend/src/applyForAccess/ApplyForAccess.js
@@ -144,92 +144,84 @@ export default function ApplyForAccess() {
 
   return (
     <div className=' contact-div'>
-      <div >
-        <div className='form-base'>
-          <h3 className='plain-h3'>Apply Here</h3>
-          <div className='w-form'>
-            <form
-              id='wf-form-Register-Form'
-              name='wf-form-Register-Form'
-              data-name='Register Form'
-              aria-label='Register Form'
-              onSubmit={handleSubmit}
-            >
-              <div className='row'>
-                <div className='col-6'>
+      <div className='form-base'>
+        <h3 className='plain-h3'>Apply Here</h3>
+        <div className='w-form'>
+          <form
+            id='wf-form-Register-Form'
+            name='wf-form-Register-Form'
+            data-name='Register Form'
+            aria-label='Register Form'
+            onSubmit={handleSubmit}
+          >
+            <div className='row'>
+              <div className='col-6'>
                 {Object.entries(formData).map(([fieldName, value], index) => (
-                index % 2 === 0 ? (
-                  <div className='form-row' key={fieldName}>
-                    <div className='form-wrap'>
-                      <input
-                        type='text'
-                        className='text-field w-input'
-                        maxLength={256}
-                        name={fieldName}
-                        value={value}
-                        placeholder={fieldName}
-                        onChange={handleChange}
-                      />
+                  index % 2 === 0 ? (
+                    <div className='form-row' key={fieldName}>
+                      <div className='form-wrap'>
+                        <input
+                          type='text'
+                          className='text-field w-input'
+                          maxLength={256}
+                          name={fieldName}
+                          value={value}
+                          placeholder={fieldName}
+                          onChange={handleChange}
+                        />
+                      </div>
                     </div>
-                  </div>
-                ) : null
-              ))}
-                </div>
-                <div className='col-6'>
-                {Object.entries(formData).map(([fieldName, value], index) => (
-                index % 2 === 1 ? (
-                  <div className='form-row' key={fieldName}>
-                    <div className='form-wrap'>
-                      <input
-                        type='text'
-                        className='text-field w-input'
-                        maxLength={256}
-                        name={fieldName}
-                        value={value}
-                        placeholder={fieldName}
-                        onChange={handleChange}
-                      />
-                    </div>
-                  </div>
-                ) : null
-              ))}
-                </div>
+                  ) : null
+                ))}
               </div>
-              
-              <button
-                type='submit'
-                className='button-submit w-button'
-                disabled={loading}
-              >
-                {loading ? (
-                  <Spinner
-                    as='span'
-                    animation='border'
-                    size='sm'
-                    role='status'
-                    aria-hidden='true'
-                  />
-                ) : (
-                  'Submit'
-                )}
-              </button>
-            </form>
-            <div className='success-message w-form-done' tabIndex='-1' role='region' aria-label='Register Form success'>
-              {successMessage ? <p>{successMessage}</p> : null}
+              <div className='col-6'>
+                {Object.entries(formData).map(([fieldName, value], index) => (
+                  index % 2 === 1 ? (
+                    <div className='form-row' key={fieldName}>
+                      <div className='form-wrap'>
+                        <input
+                          type='text'
+                          className='text-field w-input'
+                          maxLength={256}
+                          name={fieldName}
+                          value={value}
+                          placeholder={fieldName}
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+                  ) : null
+                ))}
+              </div>
             </div>
-            <div className='error-message w-form-done' tabIndex='-1' role='region' aria-label='Register Form success'>
-              {errorMessage ? <p>{errorMessage}</p> : null}
-            </div>
+
+            <button
+              type='submit'
+              className='button-submit w-button'
+              disabled={loading}
+            >
+              {loading ? (
+                <Spinner
+                  as='span'
+                  animation='border'
+                  size='sm'
+                  role='status'
+                  aria-hidden='true'
+                />
+              ) : (
+                'Submit'
+              )}
+            </button>
+          </form>
+          <div className='success-message w-form-done' tabIndex='-1' role='region' aria-label='Register Form success'>
+            {successMessage ? <p>{successMessage}</p> : null}
           </div>
-          <div className='line-break'></div>
+          <div className='error-message w-form-done' tabIndex='-1' role='region' aria-label='Register Form success'>
+            {errorMessage ? <p>{errorMessage}</p> : null}
+          </div>
         </div>
+        <div className='line-break'></div>
       </div>
-      {/* <div className='col-md-4 form-right'>
-        <h3>Unmatched Employee experience</h3>
-        <h4>
-          As a founder, it is important for me that every employee should receive a customer-grade experience. Also, it is important that I free HR from mundane tasks so that they can contribute towards bringing out the best in employees.
-        </h4>
-      </div> */}
     </div>
   );
 }
