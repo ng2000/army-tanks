3b4bd8470dfaa0ac6d725dddba8961dea2622d7f
UserName <UserEmail>
2024-04-28 16:57:29 +0530

    Renamed component file upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,22 @@
 
 // export default App;
-import {Routes , Route } from "react-router-dom"; 
-import Demo from './demo/Demo';
+import { Routes, Route } from "react-router-dom";
 import NavbarComponent from "./navbar/Navbar";
-import Solutions from './solutions/Solutions';
-import ProductFeatures from "./productFeatures/ProductFeatures";
 import Banner from './banner/Banner';
 import ApplyForAccess from "./applyForAccess/ApplyForAccess";
-import FileUpload from  "./fileUpload/FileUpload";
+import FileUploadImport from "./fileUploadImport/FileUploadImport";
 import Footer from "./footer/Footer"
 
-export default function App() { 
-   return ( 
-      <div className="App"> 
-	      <NavbarComponent/>
-        <Routes>
-          <Route path ="/" element= {<><Banner /> <FileUpload /></>}/>
-          <Route path ="/add" element= {<ApplyForAccess />}/> 
-        </Routes> 
-        <Footer />
-  </div> 
-); 
+export default function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<><Banner /></>} />
+        <Route path="/import" element={<><FileUploadImport /></>} />
+        <Route path="/add" element={<ApplyForAccess />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
---
+++ Frontend/src/fileUploadImport/FileUploadImport.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function FileUploadImport() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState('');
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    
+    if (!selectedFile) {
+      alert('Please select a file.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await axios.post('upload?path=/path/to/destination', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      setUploadStatus(response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      setUploadStatus('Error uploading file.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>File Upload</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <input type="file" onChange={handleFileChange} />
+        </div>
+        <button type="submit">Upload</button>
+      </form>
+      {uploadStatus && <p>{uploadStatus}</p>}
+    </div>
+  );
+}
