d54464c67eee5f7df21be76e5aba7474f4a760d9
UserName <UserEmail>
2024-04-28 14:55:53 +0530

    update footer

--- Frontend/src/footer/Footer.js
+++ Frontend/src/footer/Footer.js
@@ -5,17 +5,7 @@ const Footer = () => {
 
     <div >
       <footer class="bg-dark py-1">
-        {/* <ul class="nav justify-content-center mb-1">
-          <li class="nav-item"><a href="/" class="text-white nav-link px-2 ">Home</a></li>
-          <li class="nav-item"><a href="/#solutions" class="text-white nav-link px-2 ">Solutions</a></li>
-          <li class="nav-item"><a href="/#product-features" class="text-white nav-link px-2 ">Product Features</a></li>
-        </ul>
-        <ul class="nav justify-content-center mb-1">
-          <li class="nav-item"><a href="https://www.linkedin.com/company/pica-hq/" class="text-white nav-link px-2 "><i className="fab fa-linkedin-in fa-2x"></i></a></li>
-          <li class="nav-item"><a href="mailto:shivi@picahq.com" class="text-white nav-link px-2"> <i class="fa fa-envelope fa-2x"></i></a> </li>
-
-        </ul> */}
-        <p class="text-white text-center py-1">&copy; 2023 Company, Inc. All rights reserved.</p>
+        <p class="text-white text-center py-1">&copy; 2024 Company, Inc. All rights reserved.</p>
       </footer>
     </div>
   );
