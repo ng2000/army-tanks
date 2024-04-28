7e0eb1080dcd3ced30a1e4867a5aba0ea0c97210
priyanshugoel <priyanshugoel001@gmail.com>
2024-04-29 01:08:34 +0530

    Summary

--- Frontend/src/tankSummary/TankSummary.js
+++ Frontend/src/tankSummary/TankSummary.js
@@ -7,26 +7,129 @@ import "./TankSummary.css";
 export default function TankSummary() {
   const [rowData, setRowData] = useState();
   const [selectedBANo, setSelectedBANo] = useState(null);
+const fieldMap = {
+    sno: 'SNO',
+    unit: 'Unit',
+    typeOfEquipment: 'Type of Eqpt',
+    issueType: 'Issue Type',
+    baNo: 'BA No',
+    chassisNo: 'Chassis No',
+    engineOrgOH: 'Engine Org/OH',
+    engKm: 'Eng Km',
+    engHrs: 'Eng Hrs',
+    chassisKm: 'Chassis Km',
+    chassisHrs: 'Chassis Hrs',
+    tmIDone: 'TM I Done',
+    tmIDue: 'TM I Due',
+    tmIIDone: 'TM II Done',
+    tmIIDue: 'TM II Due',
+    mrIDueDt: 'MR-I Due Dt',
+    mrIDoneDt: 'MR-I Done Dt',
+    ohIDueDt: 'OH-I Due Dt',
+    ohIDoneDt: 'OH-I Done Dt',
+    mrIIDue: 'MR II Due',
+    mrIIDone: 'MR II Done',
+    ohIIDue: 'OH II Due',
+    ohIIDone: 'OH II Done',
+    serR2EOAVOR: 'SER/R2/EOA/VOR',
+    assy: 'Assy',
+    section: 'Section',
+    natureOfDefect: 'Nature of Defect',
+    demandPlacedTo: 'Demand Placed To',
+    demandNoDt: 'Demand No & Dt',
+    contNoDt: 'Cont No & Dt',
+    workOrderNoDate: 'Work Order No & Dt',
+    fwdTo: 'Fwd To',
+    since: 'Since',
+    presentStatus: 'Present Status',
+    underRepairTime: 'Under Repair Time',
+    efcRDSFired: 'EFC/RDS Fired',
+    chamberElongation: 'Chamber Elongation',
+    bore: 'Bore',
+    gunPullBackDoneDate: 'Gun Pull Back Done Date',
+    siDetails: 'SI Details',
+    fumeExtractor: 'Fume Extractor',
+    n2PurgingDueDate: 'N2 Purging Due Date',
+    n2PurgingCarriedOut: 'N2 Purging Carried Out',
+    getterActivationDoneDate: 'Getter Activation Done Date (Check Every 3 Months)'
+  };
 
   const [columnDefs, setColumnDefs] = useState([
     { field: "SNO", minWidth: 170 },
-    { field: "Unit" },
-    { field: "Type of Eqpt" },
-    { field: "Issue Type" },
-    { field: "BA No" },
+    { field: "EngineRegNo" },
+    { field: "Engine Org/OH" },
+    { field: "Date Of Fitment" },
+    { field: "Eng Hrs" },
+    { field: "Eng Km" },
   ]);
 
   const [columnDefs1, setColumnDefs1] = useState([
     { field: "SNO", minWidth: 170 },
-    { field: "Unit" },
-    { field: "Type of Eqpt" },
-    { field: "BA No" },
+    { field: "BTY Details" },
+    { field: "Life Of Battery" },
+    { field: "Date Of Issue" },
   ]);
 
   const [columnDefs2, setColumnDefs2] = useState([
     { field: "SNO", minWidth: 170 },
-    { field: "BA No" },
+    { field: "Nomenclature" },
+    { field: "Date Of Fitment" },
+    { field: "Remark" },
   ]);
+  const [columnDefs3, setColumnDefs3] = useState([
+      { field: "SNO", minWidth: 170 },
+      { field: "Eqpt Id" },
+      { field: "Tank Reg No" },
+      { field: "TM I Due" },
+      { field: "TM I Carried Out" },
+      { field: "Remark" },
+    ]);
+     const [columnDefs4, setColumnDefs4] = useState([
+          { field: "SNO", minWidth: 170 },
+          { field: "Eqpt Id" },
+          { field: "Tank Reg No" },
+          { field: "TM II Due" },
+          { field: "TM II Carried Out" },
+          { field: "Remark" },
+        ]);
+      const [columnDefs5, setColumnDefs5] = useState([
+                { field: "SNO", minWidth: 170 },
+                { field: "Eqpt Id" },
+                { field: "Tank Reg No" },
+                { field: "MR I Due" },
+                { field: "MR I Carried Out" },
+                { field: "Remark" },
+              ]);
+     const [columnDefs6, setColumnDefs6] = useState([
+               { field: "SNO", minWidth: 170 },
+               { field: "Eqpt Id" },
+               { field: "Tank Reg No" },
+               { field: "MR II Due" },
+               { field: "MR II Carried Out" },
+               { field: "Remark" },
+             ]);
+     const [columnDefs7, setColumnDefs7] = useState([
+                    { field: "SNO", minWidth: 170 },
+                    { field: "Eqpt Id" },
+                    { field: "Tank Reg No" },
+                    { field: "MR III Due" },
+                    { field: "MR III Carried Out" },
+                    { field: "Remark" },
+                  ]);
+      const [columnDefs8, setColumnDefs8] = useState([
+                         { field: "SNO", minWidth: 170 },
+                         { field: "Eqpt Id" },
+                         { field: "Tank Reg No" },
+                         { field: "BOH Due" },
+                         { field: "BOH Carried Out" },
+                         { field: "Remark" },
+                       ]);
+       const [columnDefs9, setColumnDefs9] = useState([
+                               { field: "Nomenclature", minWidth: 170 },
+                               { field: "Regd No" },
+                               { field: "Date Of Issue" },
+                               { field: "Eqpt Status" },
+                             ]);
   const defaultColDef = useMemo(() => {
     return {
       editable: true,
@@ -60,18 +163,20 @@ export default function TankSummary() {
   }, [selectedBANo, rowData]);
 
   return (
+    <div>
+    <div className="controls">
+            <label htmlFor="bano-select">Select BA No:</label>
+            <select id="bano-select" value={selectedBANo || ""} onChange={handleSelectBANo}>
+              <option value="">All</option>
+              {/* Populate options with unique BA No values */}
+              {rowData &&
+                Array.from(new Set(rowData.map(row => row["BA No"]))).map(bano => (
+                  <option key={bano} value={bano}>{bano}</option>
+                ))}
+            </select>
+    </div>
+
     <div className="center-grid">
-      <div className="controls">
-        <label htmlFor="bano-select">Select BA No:</label>
-        <select id="bano-select" value={selectedBANo || ""} onChange={handleSelectBANo}>
-          <option value="">All</option>
-          {/* Populate options with unique BA No values */}
-          {rowData &&
-            Array.from(new Set(rowData.map(row => row["BA No"]))).map(bano => (
-              <option key={bano} value={bano}>{bano}</option>
-            ))}
-        </select>
-      </div>
       <div className="ag-theme-quartz-dark gridClass center-grid">
         <AgGridReact
           rowData={filteredRowData}
@@ -82,6 +187,131 @@ export default function TankSummary() {
           onGridReady={onGridReady}
         />
       </div>
+    </div>
+
+   <div className="center-grid">
+         <div className="ag-theme-quartz-dark gridClass center-grid">
+           <AgGridReact
+             rowData={filteredRowData}
+             columnDefs={columnDefs1}
+             defaultColDef={defaultColDef}
+             className="gridClass"
+             pagination={true}
+             onGridReady={onGridReady}
+           />
+         </div>
+     </div>
+     <div className="center-grid">
+              <div className="ag-theme-quartz-dark gridClass center-grid">
+                <AgGridReact
+                  rowData={filteredRowData}
+                  columnDefs={columnDefs1}
+                  defaultColDef={defaultColDef}
+                  className="gridClass"
+                  pagination={true}
+                  onGridReady={onGridReady}
+                />
+              </div>
+          </div>
+          <div className="center-grid">
+                   <div className="ag-theme-quartz-dark gridClass center-grid">
+                     <AgGridReact
+                       rowData={filteredRowData}
+                       columnDefs={columnDefs2}
+                       defaultColDef={defaultColDef}
+                       className="gridClass"
+                       pagination={true}
+                       onGridReady={onGridReady}
+                     />
+                   </div>
+               </div>
+               <div className="center-grid">
+                        <div className="ag-theme-quartz-dark gridClass center-grid">
+                          <AgGridReact
+                            rowData={filteredRowData}
+                            columnDefs={columnDefs3}
+                            defaultColDef={defaultColDef}
+                            className="gridClass"
+                            pagination={true}
+                            onGridReady={onGridReady}
+                          />
+                        </div>
+                    </div>
+                    <div className="center-grid">
+                             <div className="ag-theme-quartz-dark gridClass center-grid">
+                               <AgGridReact
+                                 rowData={filteredRowData}
+                                 columnDefs={columnDefs4}
+                                 defaultColDef={defaultColDef}
+                                 className="gridClass"
+                                 pagination={true}
+                                 onGridReady={onGridReady}
+                               />
+                             </div>
+                  </div>
+
+                <div className="center-grid">
+                                             <div className="ag-theme-quartz-dark gridClass center-grid">
+                                               <AgGridReact
+                                                 rowData={filteredRowData}
+                                                 columnDefs={columnDefs5}
+                                                 defaultColDef={defaultColDef}
+                                                 className="gridClass"
+                                                 pagination={true}
+                                                 onGridReady={onGridReady}
+                                               />
+                                             </div>
+                                  </div>
+
+                                  <div className="center-grid">
+                                                               <div className="ag-theme-quartz-dark gridClass center-grid">
+                                                                 <AgGridReact
+                                                                   rowData={filteredRowData}
+                                                                   columnDefs={columnDefs6}
+                                                                   defaultColDef={defaultColDef}
+                                                                   className="gridClass"
+                                                                   pagination={true}
+                                                                   onGridReady={onGridReady}
+                                                                 />
+                                                               </div>
+                                                    </div>
+                                                    <div className="center-grid">
+                                                                                 <div className="ag-theme-quartz-dark gridClass center-grid">
+                                                                                   <AgGridReact
+                                                                                     rowData={filteredRowData}
+                                                                                     columnDefs={columnDefs7}
+                                                                                     defaultColDef={defaultColDef}
+                                                                                     className="gridClass"
+                                                                                     pagination={true}
+                                                                                     onGridReady={onGridReady}
+                                                                                   />
+                                                                                 </div>
+                                                                      </div>
+                                                                      <div className="center-grid">
+                                                                                                   <div className="ag-theme-quartz-dark gridClass center-grid">
+                                                                                                     <AgGridReact
+                                                                                                       rowData={filteredRowData}
+                                                                                                       columnDefs={columnDefs8}
+                                                                                                       defaultColDef={defaultColDef}
+                                                                                                       className="gridClass"
+                                                                                                       pagination={true}
+                                                                                                       onGridReady={onGridReady}
+                                                                                                     />
+                                                                                                   </div>
+                                                                                        </div>
+
+                 <div className="center-grid">
+                                              <div className="ag-theme-quartz-dark gridClass center-grid">
+                                                <AgGridReact
+                                                  rowData={filteredRowData}
+                                                  columnDefs={columnDefs9}
+                                                  defaultColDef={defaultColDef}
+                                                  className="gridClass"
+                                                  pagination={true}
+                                                  onGridReady={onGridReady}
+                                                />
+                                              </div>
+                                   </div>
     </div>
   );
 };
