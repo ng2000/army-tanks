5058ab9f1157084d461b586974bf026619480997
UserName <UserEmail>
2024-04-30 22:02:56 +0530

    logout

--- Frontend/src/navbar/Navbar.js
+++ Frontend/src/navbar/Navbar.js
@@ -1,31 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import './Navbar.css';
 
-const NavbarComponent = () => (
+const NavbarComponent = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
 
-  <Navbar expand="lg" className="navbar-bgc">
-    <Container>
-      <Navbar.Brand href="/" className="navbar-logo">
-        <img className="logo" src="../img/tankNav.png" alt="logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
-        <Nav>
+  const handleLogout = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
 
-          <Nav.Link href="/" className="navbar11_dropdown-toggle">Home</Nav.Link>
-          <Nav.Link href="/add" className="navbar11_dropdown-toggle">Add New Tank</Nav.Link>
-          <Nav.Link href="/import" className="navbar11_dropdown-toggle">Import</Nav.Link>
-          <Nav.Link href="/download" className="navbar11_dropdown-toggle">Export</Nav.Link>
-          <Nav.Link href="/analytics" className="navbar11_dropdown-toggle">Analytics</Nav.Link>
-          <Nav.Link href="/search" className="navbar11_dropdown-toggle">Search</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-        <Nav.Link href="/add" className="navbar11_dropdown-toggle"><button className="button-navigation w-button" style={{ width: "100%" }}>Add New Tank</button></Nav.Link>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+  return (
+    <Navbar expand="lg" className="navbar-bgc">
+      <Container>
+        <Navbar.Brand href="/" className="navbar-logo">
+          <img className="logo" src="../img/tankNav.png" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
+          <Nav>
+            <Nav.Link href="/" className="navbar11_dropdown-toggle">Home</Nav.Link>
+            <Nav.Link href="/add" className="navbar11_dropdown-toggle">Add New Tank</Nav.Link>
+            <Nav.Link href="/import" className="navbar11_dropdown-toggle">Import</Nav.Link>
+            <Nav.Link href="/download" className="navbar11_dropdown-toggle">Export</Nav.Link>
+            <Nav.Link href="/analytics" className="navbar11_dropdown-toggle">Analytics</Nav.Link>
+            <Nav.Link href="/search" className="navbar11_dropdown-toggle">Search</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          {isLoggedIn ? (
+            <Nav.Link href="/logout" className="navbar11_dropdown-toggle">
+              <button onClick={handleLogout} className="button-navigation w-button" style={{ width: "100%" }}>Logout</button>
+            </Nav.Link>
+          ) : (
+            <Nav.Link href="/login" className="navbar11_dropdown-toggle">
+              <button className="button-navigation w-button" style={{ width: "100%" }}>Login</button>
+            </Nav.Link>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-);
 export default NavbarComponent;
