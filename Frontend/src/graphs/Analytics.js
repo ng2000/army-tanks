5cf878181f211bc1edb301af34ed30492d2d9b00
UserName <UserEmail>
2024-05-03 22:27:38 +0530

    unit filters

--- Frontend/src/graphs/Analytics.js
+++ Frontend/src/graphs/Analytics.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import BarGraphAVGENGHrs from "./BarGraphAVGENGHrs";
 import VintageGraph from "./VintageGraph";
@@ -7,47 +7,70 @@ import PieChartEFCQTR from "./PieChartEFCQTR";
 import BarGraphMSNReliabilityENG from "./BarGraphMSNReliabilityENG";
 import PieChartMSNBRL from "./PieChartMSNBRL";
 import PieChartOrigin from "./PieChartOrigin";
+
 const Analytics = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true); // State to manage loading indicator
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [selectedUnit, setSelectedUnit] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/allData");
-        setData(response.data);
-        setLoading(false); // Set loading to false after data is fetched
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("/allData");
+                setData(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  // Display loading indicator while data is being fetched
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    const handleSelectionChangeUnit = useCallback((event) => {
+        setSelectedUnit(event.target.value);
+    }, []);
 
-  return (
-    <div className="row">
-        <div className="col-3">
-        <BarGraphAVGENGHrs data={data} />
-         <VintageGraph data={data} />
-         <PieChartOrigin data={data}></PieChartOrigin>
+    const filteredRowData = useMemo(() => {
+        if (!selectedUnit) return data;
+        return data.filter(row => {
+            const unitTrimmed = row["Unit"].trim();
+            const selectedUnitTrimmed = selectedUnit.trim();
+            return unitTrimmed === selectedUnitTrimmed;
+        });
+    }, [selectedUnit, data]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="row">
+            <div>
+                <label htmlFor="Unit-select">Unit</label>
+                <select id="Unit-select" value={selectedUnit || ""} onChange={handleSelectionChangeUnit}>
+                    {data &&
+                        Array.from(new Set(data.map(row => row["Unit"]))).map(unit => (
+                            <option key={unit} value={unit}>{unit}</option>
+                        ))}
+                </select>
+            </div>
+            <div className="col-3">
+                <BarGraphAVGENGHrs data={filteredRowData} />
+                <VintageGraph data={filteredRowData} />
+                <PieChartOrigin data={filteredRowData}></PieChartOrigin>
+            </div>
+
+            <div className="col-3">
+                <BarGraphAuthHeld data={filteredRowData}></BarGraphAuthHeld>
+                <PieChartEFCQTR data={filteredRowData}></PieChartEFCQTR>
+            </div>
+            <div className="col-3">
+                <BarGraphMSNReliabilityENG data={filteredRowData}></BarGraphMSNReliabilityENG>
+                <PieChartMSNBRL data={filteredRowData}></PieChartMSNBRL>
+            </div>
         </div>
-
-        <div className="col-3">
-            <BarGraphAuthHeld data={data}></BarGraphAuthHeld>
-            <PieChartEFCQTR data={data}></PieChartEFCQTR>
-        </div>
-        <div className="col-3">
-            <BarGraphMSNReliabilityENG data={data}></BarGraphMSNReliabilityENG>
-            <PieChartMSNBRL data={data}></PieChartMSNBRL>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Analytics;
